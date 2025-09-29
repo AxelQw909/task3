a53ded9c831d869d4152061f416050f8b10e6f25
AxelQw909 <dart58215@gmail.com>
2025-09-29 11:11:44 +0500

    доделать 4 задание

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Report;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function index(){
-       
+        $reports= Report::all();
+        return view('report.index', compact('reports'));
     }
 }
